ca374348584192ea18e49d695ac223f7c624a4a1
Al - Amin <alamin.binmannan@shurjomukhi.com.bd>
2022-11-28 18:29:33 +0600

    configure pom to upload maven central

--- src/test/java/bd/com/shurjopay/plugin/ShurjoPaySpringTest.java
+++ src/test/java/bd/com/shurjopay/plugin/ShurjoPaySpringTest.java
@@ -1,4 +1,4 @@
-package com.shurjopay.plugin;
+package bd.com.shurjopay.plugin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
