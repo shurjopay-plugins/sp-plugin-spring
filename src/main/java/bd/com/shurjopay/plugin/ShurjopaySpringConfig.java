8158fd79e4ff98042560fb2c61ed919a8b4567bf
Al - Amin <alamin.binmannan@shurjomukhi.com.bd>
2022-12-07 15:11:59 +0600

    java-docs writing completed

--- src/main/java/bd/com/shurjopay/plugin/ShurjopaySpringConfig.java
+++ src/main/java/bd/com/shurjopay/plugin/ShurjopaySpringConfig.java
@@ -7,21 +7,20 @@ import org.springframework.context.annotation.Configuration;
 import bd.com.shurjopay.plugin.model.ShurjopayConfig;
 
 /**
- * This service provides all the services of shurjoPay For using this service,
- * You have to provide below namespace's values in spring default properties
- * file <code>
+ * Configures auto-wired shurjopay spring bean,
+ * Developers need to provide below namespace's values in spring default properties.
+ * <code>
  * shurjopay.username,
  * shurjopay.password,
  * shurjopay.shurjopay-api,
  * shurjopay.sp-callback
  * </code>
  * 
- * @author Al-Amin
+ * @author Al - Amin
  * @since 2022-10-16
  */
 @Configuration
 public class ShurjopaySpringConfig {
-
 	@Value("${shurjopay.username}")
 	private String username;
 
@@ -34,14 +33,25 @@ public class ShurjopaySpringConfig {
 	@Value("${shurjopay.sp-callback}")
 	private String callbackUrl;
 
+	/**
+	 * Instantiates Shurjopay spring bean to serve auto-wired injection.
+	 * @return {@link Shurjopay} spring bean with spring default properties configurations for shurjopay.
+	 */
 	@Bean
 	public Shurjopay shurjoPay() {
 		return new Shurjopay(shurjopayConfig());
 	}
 
+	/**
+	 * Loads shurjopay configurations.
+	 * @return {@link ShurjopayConfig} with shurjopay configuration which are comes from default properties.
+	 */
 	@Bean
 	public ShurjopayConfig shurjopayConfig() {
-		return new ShurjopayConfig().setUsername(username).setPassword(password).setApiBaseUrl(apiBaseUrl)
-				.setCallbackUrl(callbackUrl);
+		return new ShurjopayConfig()
+				   .setUsername(username)
+				   .setPassword(password)
+				   .setApiBaseUrl(apiBaseUrl)
+				   .setCallbackUrl(callbackUrl);
 	}
 }
